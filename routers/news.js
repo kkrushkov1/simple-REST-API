8d73115f8336f559c565b3f0fee0986a98c0e0ea
kkrushkov <k.krushkov1@gmail.com>
2023-03-09 11:22:37 +0200

    Updated router

--- routers/news.js
+++ routers/news.js
@@ -2,15 +2,6 @@ const express = require("express");
 const router = express.Router();
 const NewsController = require("../controllers/news");
 
-// router.get("/", async (req, res) => {
-//   try {
-//     const news = await New.find();
-//     res.json(news);
-//   } catch (err) {
-//     res.status(500).json({ message: err.message });
-//   }
-// });
-
 router.get("/", NewsController.getAllNews);
 
 router.get("/:id", NewsController.getById);
@@ -21,32 +12,8 @@ router.post(
   NewsController.createSingleNew
 );
 
-// router.post("/", async (req, res) => {
-//     try {
-//       const n1 = await New.create(req.body);
-//       res.status(201).json(n1);
-//     } catch (err) {
-//       res.status(400).json({ message: err.message });
-//     }
-//   });
-
 router.put("/:id", NewsController.updateANew);
 
 router.delete("/:id", NewsController.deleteANew);
-
-// async function getSingleNew(req, res, next) {
-//   let singleNew;
-
-//   try {
-//     singleNew = await New.findById(req.params.id);
-//     if (singleNew == null) {
-//       return res.status(404).json({ message: "Cannot find subscriber" });
-//     }
-//   } catch (err) {
-//     return res.status(500).json({ message: err.message });
-//   }
-//   res.singleNew = singleNew;
-//   next();
-// }
 
 module.exports = router;
